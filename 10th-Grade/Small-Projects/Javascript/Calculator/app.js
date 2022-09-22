b9623ab7dc3f3722c9b0b7636424204088d40d08
Viraj Bagga <90282313+Viraj-Bagga@users.noreply.github.com>
2022-09-22 18:03:12 -0400

    Update app.js

--- 10th-Grade/Small-Projects/Javascript/Calculator/app.js
+++ 10th-Grade/Small-Projects/Javascript/Calculator/app.js
@@ -1,1 +1,60 @@
 
+const $input = document.querySelector("input");
+
+document.querySelectorAll(".new__key").forEach( el => {
+    el.onclick = () => $input.value = $input.value !== "0" ? $input.value + el.innerText : el.innerText;
+})
+
+const buffer = [];
+
+const opCallback = opName => () => {
+    let currentVal = parseFloat($input.value);
+
+    if(opName === "percent"){
+        currentVal *= 0.01;
+        $input.value = currentVal;
+    }
+    else{
+        if(buffer && buffer.lenght) {
+            buffer.push({value: currentVal});
+
+            const result = evaluate(buffer);
+
+            buffer.push({value: result});
+            buffer.push({value: opName});
+
+            $input.value = "";
+        }
+        else{
+            buffer.push({ value: currentVal});
+            buffer.push({value: opName});
+
+            $input.value = "";
+        }
+    }
+}
+
+const evaluate = buffer => {
+    const secondOperand = buffer.pop().value;
+    const operator = buffer.pop().value;
+    const firstOperand = buffer.pop().value;
+
+    switch(operator) {
+        case "add":
+            return firstOperand + secondOperand;
+            break;
+        case "subtract":
+            return firstOperand - secondOperand;
+            break;
+        case "multiply":
+            return firstOperand * secondOperand;
+            break;
+        case "divide":
+            return firstOperand / secondOperand;
+            break;
+    }
+}
+
+for (const opName of ["add", "subtract", "multiply", "devide", "percent"]) {
+    document.querySelector(`.op__key[op=${opName}]`).onclick = opCallback(opName);
+}
