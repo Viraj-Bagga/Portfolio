cff64e80dca7bb0965b47f57a94d9437ac6012c2
Viraj Bagga <90282313+Viraj-Bagga@users.noreply.github.com>
2022-09-22 18:14:54 -0400

    Update app.js

--- 10th-Grade/Small-Projects/Javascript/Calculator/app.js
+++ 10th-Grade/Small-Projects/Javascript/Calculator/app.js
@@ -1,60 +1,27 @@
+let display = document.getElementById('display');
 
-const $input = document.querySelector("input");
+let buttons = Array.from(document.getElementsByClassName('button'));
 
-document.querySelectorAll(".new__key").forEach( el => {
-    el.onclick = () => $input.value = $input.value !== "0" ? $input.value + el.innerText : el.innerText;
-})
-
-const buffer = [];
-
-const opCallback = opName => () => {
-    let currentVal = parseFloat($input.value);
-
-    if(opName === "percent"){
-        currentVal *= 0.01;
-        $input.value = currentVal;
-    }
-    else{
-        if(buffer && buffer.lenght) {
-            buffer.push({value: currentVal});
-
-            const result = evaluate(buffer);
-
-            buffer.push({value: result});
-            buffer.push({value: opName});
-
-            $input.value = "";
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
         }
-        else{
-            buffer.push({ value: currentVal});
-            buffer.push({value: opName});
-
-            $input.value = "";
-        }
-    }
-}
-
-const evaluate = buffer => {
-    const secondOperand = buffer.pop().value;
-    const operator = buffer.pop().value;
-    const firstOperand = buffer.pop().value;
-
-    switch(operator) {
-        case "add":
-            return firstOperand + secondOperand;
-            break;
-        case "subtract":
-            return firstOperand - secondOperand;
-            break;
-        case "multiply":
-            return firstOperand * secondOperand;
-            break;
-        case "divide":
-            return firstOperand / secondOperand;
-            break;
-    }
-}
-
-for (const opName of ["add", "subtract", "multiply", "devide", "percent"]) {
-    document.querySelector(`.op__key[op=${opName}]`).onclick = opCallback(opName);
-}
+    });
+});
