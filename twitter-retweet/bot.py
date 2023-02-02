eaef47288b32e330cd29b105dba15fdcf3e1b1a7
Viraj Bagga <90282313+Viraj-Bagga@users.noreply.github.com>
2023-02-02 08:46:54 -0500

    Update bot.py

--- twitter-retweet/bot.py
+++ twitter-retweet/bot.py
@@ -8,3 +8,16 @@ API_KEY = os.dotenv('twitter_api_key')
 API_SECRET = os.dotenv('twitter_api_secret')
 ACCESS_TOKEN = os.dotenv('twitter_access_token')
 TOKEN_SECRET = os.dotenv('twitter_token_secret')
+
+twitter_oauth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+twitter_oauth.set_access_token(ACCESS_TOKEN, TOKEN_SECRET)
+
+twitter_api = tweepy.API(twitter_oauth)
+
+print(twitter_api.verify_credentials())
+print("Succesfully logged in")
+except tweepy.TweepError as e:
+    print(e)
+except Expection as e:
+    print(e)
+    
