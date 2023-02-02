33f770d1ccc90a906b2193eba47f3b2560f399f1
Viraj Bagga <90282313+Viraj-Bagga@users.noreply.github.com>
2023-02-02 09:42:07 -0500

    Update bot.py

--- twitter-retweet/bot.py
+++ twitter-retweet/bot.py
@@ -1,24 +1,29 @@
 import tweepy
-import os 
+import os
 from dotenv import load_dotenv
+import json
+from streamlistener import StreamListener
 
 load_dotenv()
 
-API_KEY = os.dotenv('twitter_api_key')
-API_SECRET = os.dotenv('twitter_api_secret')
-ACCESS_TOKEN = os.dotenv('twitter_access_token')
-TOKEN_SECRET = os.dotenv('twitter_token_secret')
+API_KEY = os.getenv('twitter_api_key')
+API_SECRET = os.getenv('twitter_api_secret')
+ACCESS_TOKEN = os.getenv('twitter_access_token')
+TOKEN_SECRET = os.getenv('twitter_token_secret')
 
-twitter_oauth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-twitter_oauth.set_access_token(ACCESS_TOKEN, TOKEN_SECRET)
+oauth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+oauth.set_access_token(ACCESS_TOKEN, TOKEN_SECRET)
 
-twitter_api = tweepy.API(twitter_oauth)
+api = tweepy.API(oauth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
 
 try:
     print(twitter_api.verify_credentials())
-    print("Succesfully logged in")
+    print("Successfully logged in")
 except tweepy.TweepError as e:
     print(e)
-except Expection as e:
+except Exception as e:
     print(e)
-    
+
+tweets_listener = StreamListener(api)
+tweet_stream = tweepy.Stream(api.auth, tweets_listener)
+tweet_stream.filter(track=["#100DaysOfCode", "#30DaysOfCode"], languages=["en"])
