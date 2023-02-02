94274ecb80f0e7a22d44784a463399d67ad35b35
Viraj Bagga <90282313+Viraj-Bagga@users.noreply.github.com>
2023-02-02 08:30:58 -0500

    Update bot.py

--- twitter-retweet/bot.py
+++ twitter-retweet/bot.py
@@ -1,1 +1,10 @@
+import tweepy
+import os 
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_KEY = os.dotenv('twitter_api_key')
+API_SECRET = os.dotenv('twitter_api_secret')
+ACCESS_TOKEN = os.dotenv('twitter_access_token')
+TOKEN_SECRET = os.dotenv('twitter_token_secret')
